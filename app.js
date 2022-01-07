8a1a799d9134f5821b0cf947fe0f11dab57d1877
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2022-01-07 10:54:27 +0100

    node update

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-const _ = require("lodash");
+const http = require("http");
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home Page");
+  }
+  if (req.url === "/about") {
+    res.end("About page");
+  }
+  res.end("Error page");
+});
+
+server.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
+
+// const { readFile } = require("fs");
+
+// console.log("started a first task");
+
+// // Check file path
+
+// readFile("./content/first.txt", "utf8", (err, result) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(result);
+//   console.log("completed first task");
+// });
+// console.log("starting next task");
